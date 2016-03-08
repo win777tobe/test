80bcc950b2155fd858becde6dbd68e8369b1c342
Dwayne <win777tobe@gmail.com>
2016-03-08 15:35:08 +0900

    브랜치 커밋

--- kkkkkkk/src/test/TestClass.java
+++ kkkkkkk/src/test/TestClass.java
@@ -4,5 +4,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("TEST1");
+		
+		System.out.println("Branch Version into Master");
 	}
 }
