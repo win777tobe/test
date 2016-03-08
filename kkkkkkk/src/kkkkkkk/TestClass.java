f7cacdb72caa4bd99e801e26aa7665677a302164
Dwayne <win777tobe@gmail.com>
2016-03-08 09:15:56 +0900

    클래스명 변경

--- kkkkkkk/src/kkkkkkk/TestClass.java
+++ kkkkkkk/src/kkkkkkk/TestClass.java
@@ -1,10 +1,8 @@
 package kkkkkkk;
 
-public class jjjjjjjj {
+public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		
-		
 		System.out.println("TEST1");
 	}
 }
